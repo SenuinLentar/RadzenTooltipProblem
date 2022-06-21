85e331266f7e63ce79e9dedf6bfb056283be41f1
Maxime Lavergne <mlavergne@jeanlutzsa.com>
2022-06-21 16:20:33 +0200

    Changing datas for points location problems

--- RadzenTooltipProblem/Pages/Index.razor.cs
+++ RadzenTooltipProblem/Pages/Index.razor.cs
@@ -10,25 +10,12 @@ namespace RadzenTooltipProblem.Pages
         {
             ChartData = new List<GenericJobData>()
             {
-                new GenericJobData(new DateTime(2022, 06, 20), 13),
-                new GenericJobData(new DateTime(2022, 06, 23), 10),
-                new GenericJobData(new DateTime(2022, 06, 24), 23),
-                new GenericJobData(new DateTime(2022, 06, 25), 10),
-                new GenericJobData(new DateTime(2022, 06, 26), 16),
-                new GenericJobData(new DateTime(2022, 06, 27), 0),
-                new GenericJobData(new DateTime(2022, 06, 30), 10),
-                new GenericJobData(new DateTime(2022, 06, 1), 5),
-                new GenericJobData(new DateTime(2022, 07, 3), 7),
-                new GenericJobData(new DateTime(2022, 06, 5), 10),
-                new GenericJobData(new DateTime(2022, 06, 6), 13),
-                new GenericJobData(new DateTime(2022, 06, 7), 19),
-                new GenericJobData(new DateTime(2022, 06, 11), 51),
-                new GenericJobData(new DateTime(2022, 06, 15), 1),
-                new GenericJobData(new DateTime(2022, 06, 16), 13),
-                new GenericJobData(new DateTime(2022, 06, 17), 7),
-                new GenericJobData(new DateTime(2022, 06, 18), 18),
-                new GenericJobData(new DateTime(2022, 06, 19), 16),
-                new GenericJobData(new DateTime(2022, 06, 22), 7),
+                new GenericJobData(new DateTime(2022, 06, 14), 75),
+                new GenericJobData(new DateTime(2022, 06, 15), 89),
+                new GenericJobData(new DateTime(2022, 06, 16), 59),
+                new GenericJobData(new DateTime(2022, 06, 17), 46),
+                new GenericJobData(new DateTime(2022, 06, 20), 67),
+                new GenericJobData(new DateTime(2022, 06, 21), 17),
             };
 
             //ChartData = new List<GenericJobData>();
@@ -57,3 +44,27 @@ namespace RadzenTooltipProblem.Pages
         }
     }
 }
+
+
+//ChartData = new List<GenericJobData>()
+//            {
+//                new GenericJobData(new DateTime(2022, 06, 20), 13),
+//                new GenericJobData(new DateTime(2022, 06, 23), 10),
+//                new GenericJobData(new DateTime(2022, 06, 24), 23),
+//                new GenericJobData(new DateTime(2022, 06, 25), 10),
+//                new GenericJobData(new DateTime(2022, 06, 26), 16),
+//                new GenericJobData(new DateTime(2022, 06, 27), 0),
+//                new GenericJobData(new DateTime(2022, 06, 30), 10),
+//                new GenericJobData(new DateTime(2022, 06, 1), 5),
+//                new GenericJobData(new DateTime(2022, 07, 3), 7),
+//                new GenericJobData(new DateTime(2022, 06, 5), 10),
+//                new GenericJobData(new DateTime(2022, 06, 6), 13),
+//                new GenericJobData(new DateTime(2022, 06, 7), 19),
+//                new GenericJobData(new DateTime(2022, 06, 11), 51),
+//                new GenericJobData(new DateTime(2022, 06, 15), 1),
+//                new GenericJobData(new DateTime(2022, 06, 16), 13),
+//                new GenericJobData(new DateTime(2022, 06, 17), 7),
+//                new GenericJobData(new DateTime(2022, 06, 18), 18),
+//                new GenericJobData(new DateTime(2022, 06, 19), 16),
+//                new GenericJobData(new DateTime(2022, 06, 22), 7),
+//            };
